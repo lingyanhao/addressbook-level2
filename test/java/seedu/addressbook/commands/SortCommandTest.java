c985ef8d98ea6623d2bc0eee89994e4fffdc1807
lingyanhao <lingyanhao@live.com>
2019-02-09 19:27:22 +0800

    Change SortCommandTest, use library without jupiter

--- test/java/seedu/addressbook/commands/SortCommandTest.java
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -8,7 +8,7 @@ import seedu.addressbook.util.TestUtil;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class SortCommandTest {
 
